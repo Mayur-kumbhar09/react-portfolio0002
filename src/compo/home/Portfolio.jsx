2168e424ec78634b0b5cea215cbeecab2bd67288
Mayur Hanmant Kumbhar <84976636+Mayur-kumbhar09@users.noreply.github.com>
2026-09-01 12:04:05 +0530

    added responsiveness
    added responsiveness

--- src/compo/home/Portfolio.jsx
+++ src/compo/home/Portfolio.jsx
@@ -610,7 +610,7 @@ export default function Portfolio() {
                   component="h1"
                   sx={{
                     fontSize: {
-                      xs: "4.2rem",
+                      xs: "3.8rem",
                       sm: "6rem",
                       md: "9rem",
                       lg: "11rem",
